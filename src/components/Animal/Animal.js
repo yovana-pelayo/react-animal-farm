decd84aaa46589ef5bcd14ee20de1b526ae0f9f3
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-03-08 14:16:54 -0800

    animals are displaying across page. corrected style syntax

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -3,7 +3,7 @@ import './Animal.css';
 
 export default function Animal(props) {
   return (
-    <div className="animal">
+    <div className="animal" style={{ top: props.top, left: props.left }}>
       {props.type} {props.name} {props.says}
       <img alt={props.name} src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
       <span className="name">{props.name}</span>
